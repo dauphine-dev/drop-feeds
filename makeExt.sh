fd99e5ca7273faa96583108da50c414e0bb160d2
dauphine-dev <dauphinedev@serre-fr.org>
2018-02-28 18:58:53 +0100

    Add script to create addon zip file Update version to 0.2.3

---
+++ makeExt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BRANCH=${PWD##*/}
+rm -rf ../$BRANCH-ext/
+cp -fr . ../$BRANCH-ext/
+rm -rf ../$BRANCH-ext/themes/dauphine/img-src
+rm -rf ../$BRANCH-ext/themes/legacy/img-src
+rm -rf ../$BRANCH-ext/dropfeeds.code-workspace
+rm -rf ../$BRANCH-ext/README.md
+rm -rf ../$BRANCH-ext/.git/
+rm -rf ../$BRANCH-ext/.vscode/
+rm -rf ../$BRANCH-ext/.eslintrc.json
+rm -rf ../$BRANCH-ext/.gitignore
+cd ../$BRANCH-ext/
+zip -r ../$BRANCH.zip *
+cd ..
+cp -fr $BRANCH.zip drop-feeds.zip
+rm -rf $BRANCH-ext/
