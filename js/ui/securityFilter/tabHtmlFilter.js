731516c704cda23680eee9fc71eae0332b1ceef0
dauphine-dev <dauphinedev@serre-fr.org>
2018-06-27 12:06:43 +0200

    Security filters option page

--- js/ui/securityFilter/tabHtmlFilter.js
+++ js/ui/securityFilter/tabHtmlFilter.js
@@ -5,14 +5,28 @@ class TabHtmlFilter { /*exported TabHtmlFilter*/
 
   constructor() {
     this._allowedTagList = null;
+    this._selectedCell = null;
     this._updateLocalizedStrings();
     document.getElementById('htmlFilterAddButton').addEventListener('click', (e) => { this._htmlFilterAddButtonOnClicked_event(e); });
     document.getElementById('htmlFilterRemoveButton').addEventListener('click', (e) => { this._htmlFilterRemoveButtonOnClicked_event(e); });
+    document.getElementById('htmlFilterRestoreDefaultButton').addEventListener('click', (e) => { this._htmlFilterRestoreDefaultButtonOnClicked_event(e); });
+
+    document.getElementById('htmlFilterAddDialogCancel').addEventListener('click', (e) => { this._htmlFilterAddDialogCancelOnClicked_event(e); });
+    document.getElementById('htmlFilterAddDialogOk').addEventListener('click', (e) => { this._htmlFilterAddDialogOkOnClicked_event(e); });
+
   }
 
   async init_async() {
     this._allowedTagList = await LocalStorageManager.getValue_async('allowedHtmlElementsList', DefaultValues.getAllowedTagList());
+    this._allowedTagList.sort();
+    this._updateHtmlTagsTable();
+  }
+
+  _updateHtmlTagsTable() {
     let tableElement = document.getElementById('allowedHtmlElementsList');
+    let range = document.createRange();
+    range.selectNodeContents(tableElement);
+    range.deleteContents();
     this._allowedTagList.map(tag => {
       let lineElement = document.createElement('tr');
       lineElement.addEventListener('click', (e) => { this.rowSelected_event(e); });
@@ -29,14 +43,11 @@ class TabHtmlFilter { /*exported TabHtmlFilter*/
     document.getElementById('allowedHtmlElements').textContent = browser.i18n.getMessage('optAllowedHtmlElements');
     document.getElementById('htmlFilterAddButton').textContent = browser.i18n.getMessage('optHtmlFilterAddButton');
     document.getElementById('htmlFilterRemoveButton').textContent = browser.i18n.getMessage('optHtmlRemoveAddButton');
-  }
-
-  async _scriptManagerButtonOnClicked_event() {
-    await browser.tabs.create({ url: '/html/userScripts.html', active: true });
-  }
-
-  async _securityFilterButtonOnClicked_event() {
-    await browser.tabs.create({ url: '/html/securityFilter.html', active: true });
+    document.getElementById('htmlFilterRestoreDefaultButton').textContent = browser.i18n.getMessage('optHtmlFilterRestoreDefaultButton');
+    document.getElementById('htmlFilterAddDialogTitle').textContent = browser.i18n.getMessage('optHtmlFilterAddDialogTitle');
+    document.getElementById('htmlFilterAddDialogElementToAllow').textContent = browser.i18n.getMessage('optHtmlFilterAddDialogElementToAllow');
+    document.getElementById('htmlFilterAddDialogCancel').textContent = browser.i18n.getMessage('optHtmlFilterAddDialogCancel');
+    document.getElementById('htmlFilterAddDialogOk').textContent = browser.i18n.getMessage('optHtmlFilterAddDialogOk');
   }
 
   async rowSelected_event(event) {
@@ -44,14 +55,55 @@ class TabHtmlFilter { /*exported TabHtmlFilter*/
     rowList.map(row => row.classList.remove('selectedRow'));
     let target = (event.target.tagName.toLowerCase() == 'td' ? event.target.parentNode : event.target);
     target.classList.add('selectedRow');
+    this._selectedCell = target.children[0];
   }
 
   async _htmlFilterAddButtonOnClicked_event() {
-
+    let htmlFilterAddDialog = document.getElementById('htmlFilterAddDialog');
+    let xMax = window.scrollX + window.innerWidth - htmlFilterAddDialog.offsetWidth;
+    let yMax = window.scrollY + window.innerHeight - htmlFilterAddDialog.offsetHeight;
+    let rectHtmlFilterAddButton = document.getElementById('htmlFilterAddButton').getBoundingClientRect();
+    let xPos = Math.max(0, Math.min(rectHtmlFilterAddButton.right, xMax));
+    let yPos = Math.max(0, Math.min(rectHtmlFilterAddButton.bottom, yMax));
+    htmlFilterAddDialog.style.left = xPos + 'px';
+    htmlFilterAddDialog.style.top = yPos + 'px';
+    document.getElementById('htmlFilterAddDialogTag').value = '';
+    htmlFilterAddDialog.classList.remove('hide');
+    htmlFilterAddDialog.classList.add('show');
   }
 
   async _htmlFilterRemoveButtonOnClicked_event() {
+    if (this._selectedCell) {
+      this._allowedTagList = this._allowedTagList.filter(tag => tag != this._selectedCell.textContent);
+      this._allowedTagList.sort();
+      LocalStorageManager.setValue_async('allowedHtmlElementsList', this._allowedTagList);
+      this._updateHtmlTagsTable();
+    }
+  }
 
+  async _htmlFilterRestoreDefaultButtonOnClicked_event() {
+    this._allowedTagList = DefaultValues.getAllowedTagList();
+    this._allowedTagList.sort();
+    this._allowedTagList.sort();
+    LocalStorageManager.setValue_async('allowedHtmlElementsList', this._allowedTagList);
+    this._updateHtmlTagsTable();
+  }
+
+  async _htmlFilterAddDialogCancelOnClicked_event() {
+    let htmlFilterAddDialog = document.getElementById('htmlFilterAddDialog');
+    htmlFilterAddDialog.classList.remove('show');
+    htmlFilterAddDialog.classList.add('hide');
+  }
+
+  async _htmlFilterAddDialogOkOnClicked_event() {
+    let htmlFilterAddDialogTag = document.getElementById('htmlFilterAddDialogTag');
+    this._allowedTagList.push(htmlFilterAddDialogTag.value.trim().toLowerCase());
+    this._allowedTagList.sort();
+    LocalStorageManager.setValue_async('allowedHtmlElementsList', this._allowedTagList);
+    let htmlFilterAddDialog = document.getElementById('htmlFilterAddDialog');
+    htmlFilterAddDialog.classList.remove('show');
+    htmlFilterAddDialog.classList.add('hide');
+    this._updateHtmlTagsTable();
   }
 
 }
