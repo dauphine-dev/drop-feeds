4da7459b16d20b7221a26ceaf02fc467e9d633af
Jeff Thieleke <github@thieleke.com>
2018-08-26 13:11:37 -0500

    Fix spacing

--- js/tools/feedParser.js
+++ js/tools/feedParser.js
@@ -615,7 +615,7 @@ class FeedParser { /*exported FeedParser*/
   static _disableTags(text, tagToDisableList, hide) {
     for (let tag of tagToDisableList) {
       if(!tag)
-          continue;
+        continue;
       text = text.replace(new RegExp('<' + tag, 'gi'), '<' + tag + '-blocked-by-dropfeeds' + (hide ? ' style="display:none"' : ''));
       text = text.replace(new RegExp('<\\s*/' + tag, 'gi'), '</' + tag + '-blocked-by-dropfeeds');
     }
