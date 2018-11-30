3601181f84e52941d2d5634133f17ddf28175d87
dauphine-dev <dauphinedev@serre-fr.org>
2018-11-30 18:31:56 +0100

    self status bar message cleanup

--- js/ui/sidebar/feedsStatusBar.js
+++ js/ui/sidebar/feedsStatusBar.js
@@ -2,20 +2,24 @@
 'use strict';
 class FeedsStatusBar { /*exported FeedsStatusBar*/
   static get instance() { return (this._instance = this._instance || new this()); }
+  
+  constructor() {
+    this._messageTimeOut = setTimeout(() => { }, 1);
+  }
 
   set workInProgress(workInProgress) {
     FeedsTopMenu.instance.workInProgress = workInProgress;
-    if (workInProgress)
-    {
+    if (workInProgress) {
       document.getElementById('statusButton').classList.add('statusButtonUpdating');
     }
-    else
-    {
+    else {
       document.getElementById('statusButton').classList.remove('statusButtonUpdating');
     }
   }
 
   set text(text) {
+    clearTimeout(this._messageTimeOut);
     BrowserManager.setInnerHtmlById('statusText', text);
+    this._messageTimeOut = setTimeout(() => { BrowserManager.setInnerHtmlById('statusText', ''); }, 2000);
   }
 }
