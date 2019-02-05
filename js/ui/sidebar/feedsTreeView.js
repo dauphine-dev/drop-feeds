d21d2549f33fe96bcffe075af2525edeae7b3386
dauphine-dev <dauphinedev@serre-fr.org>
2019-02-05 11:46:34 +0100

    Fix right click on folder icons

--- js/ui/sidebar/feedsTreeView.js
+++ js/ui/sidebar/feedsTreeView.js
@@ -234,6 +234,13 @@ class FeedsTreeView { /*exported FeedsTreeView*/
       afterFolder.addEventListener('drop', (e) => { this._afterFolderOnDrop_event(e); });
     }
 
+    let labelForList = document.querySelectorAll('label');
+    for (let label of labelForList) {
+      label.addEventListener('contextmenu', (e) => { this._folderOnRightClicked_event(e); });
+    }
+
+
+
   }
   
   async _feedOnRightClicked_event(event) {
