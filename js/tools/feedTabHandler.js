b3095c182949edba94a27b7e9e5cfc7376f27559
dauphine-dev <dauphinedev@serre-fr.org>
2019-02-22 10:52:28 +0100

    Improve error management in FeedTabHandler

--- js/tools/feedTabHandler.js
+++ js/tools/feedTabHandler.js
@@ -4,7 +4,9 @@ class FeedTabHandler { /*exported FeedTabHandler*/
   static get instance() { return (this._instance = this._instance || new this()); }
 
   constructor() {
-    this._last = { details: null, date: new Date() };
+    this._errorList = [];
+    this._last = { timeStamp: 0 };
+    this._lastNoTime = { timeStamp: 0 };
     this._isFeedRegex = /rss|feed|atom|syndicate|xml|json/i;
     this._handlesFeedTab = DefaultValues.handlesFeedTab;
     let filter = { url: [{ urlMatches: this._isFeedRegex.source }] };
@@ -18,33 +20,48 @@ class FeedTabHandler { /*exported FeedTabHandler*/
   }
 
   async _interceptFeedDocuments_async(details) {
+    let detailsNoTime = details;
+    detailsNoTime.timeStamp = 0;
     if (details.frameId != 0) { return; } //we check only the main frame
     let isFeed = details.url.match(this._isFeedRegex);
     if (isFeed) {
-      if (details.url.includes('addons.mozilla.org')) { return; }
+      if (this._isInErrorList(detailsNoTime)) { return; }
       try {
-        let now = new Date();
-        if (this._isInProgress(details, now)) { return; }
-        this._last = { details: details, date: now };
+        if (this._isInProgress(details, detailsNoTime)) { return; }
+        this._last = details;
+        this._lastNoTime = detailsNoTime;
         let feed = await Feed.newByUrl(details.url);
-        let tabUrl = details.url;
         browser.tabs.update(details.tabId, { url: 'about:blank' }); // avoid dialog box "Opening"
         await feed.update_async();
         if (feed.error) {
-          browser.tabs.update(details.tabId, { url: tabUrl }); // load original page
+          this._errorList.push(detailsNoTime);
+          this._errorList = this._errorList.slice(-1024); // limit array size to last 1024 entries
+          browser.tabs.update(details.tabId, { url: details.url }); // load original page
           return;
         }
-        let feedHtmlUrl = await feed.getDocUrl_async(tabUrl);
+        let feedHtmlUrl = await feed.getDocUrl_async(details.url);
         browser.tabs.update(details.tabId, { url: feedHtmlUrl }); // load feed preview
       }
       catch (e) { }
     }
   }
 
-  _isInProgress(details, now) {
-    let sameAsPrev = (this._last.details && this._last.details.tabId == details.tabId && this._last.details.url == details.url);
-    let expirationDate = new Date(this._last.date.getTime() + 2000);
-    let isExpired = (now > expirationDate);
+  _isInErrorList(detailsNoTime) {
+    //let isInErrorList = this._errorList.includes(detailsNoTime);
+    let isInErrorList = this._errorList.some(d =>
+      d.url == detailsNoTime.url &&
+      d.timeStamp == detailsNoTime.timeStamp &&
+      d.frameId == detailsNoTime.frameId &&
+      d.parentFrameId == detailsNoTime.parentFrameId &&
+      d.tabId == detailsNoTime.tabId &&
+      d.windowId == detailsNoTime.windowId
+    );
+    return isInErrorList;
+  }
+
+  _isInProgress(details, detailsNoTime) {
+    let isExpired = (details.timeStamp - this._last.timeStamp > 2000);
+    let sameAsPrev = (detailsNoTime == this._lastNoTime);
     let inProgress = !isExpired && sameAsPrev;
     return inProgress;
   }
