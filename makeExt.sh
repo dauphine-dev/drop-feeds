37fedfce02a9d2ef3198d70be77ff63c1a30a646
dauphine-dev <dauphinedev@serre-fr.org>
2023-03-05 10:37:52 +0100

    update version v2 manisfest as default merge for font size fix

--- makeExt.sh
+++ makeExt.sh
@@ -47,23 +47,24 @@ rm -rf .git/
 rm -rf .vscode/
 rm -rf .eslintrc.json
 rm -rf .gitignore
-#Creating for zip manifest v2 version
-cd $INITIAL_DIR
-cp -p manifestv2.json manifest.json
-cp -p manifestv2.json ../_ext/$BRANCH/manifest.json
-cd ../_ext/$BRANCH
-VERSION=$(jq -r '.version' manifest.json)
-rm -rf ../$NAME-$VERSION-mv2.zip
-zip -r ../$NAME-$VERSION-mv2.zip * >/dev/null
 #Creating for zip manifest v3 version
 cd $INITIAL_DIR
 cp -p manifestv3.json manifest.json
 cp -p manifestv3.json ../_ext/$BRANCH/manifest.json
-sleep 2
+sleep 1
 cd ../_ext/$BRANCH
 VERSION=$(jq -r '.version' manifest.json)
 rm -rf ../$NAME-$VERSION-mv3.zip
 zip -r ../$NAME-$VERSION-mv3.zip * >/dev/null
+#Creating for zip manifest v2 version
+cd $INITIAL_DIR
+cp -p manifestv2.json manifest.json
+cp -p manifestv2.json ../_ext/$BRANCH/manifest.json
+sleep 1
+cd ../_ext/$BRANCH
+VERSION=$(jq -r '.version' manifest.json)
+rm -rf ../$NAME-$VERSION-mv2.zip
+zip -r ../$NAME-$VERSION-mv2.zip * >/dev/null
 #finishing...
 cd $INITIAL_DIR
 echo "Extension created"
